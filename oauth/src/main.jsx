937d502f4f13d3ce68d1eea62522c74f26230783
Sankalpa <joshisankalpa2@gmail.com>
2024-07-18 18:17:43 +0545

    oauth with google

--- oauth/src/main.jsx
+++ oauth/src/main.jsx
@@ -5,8 +5,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='419679539772-9qdng7lebu4li8jtgi1cne59728m8849.apps.googleusercontent.com'>
+    {/* <GoogleOAuthProvider clientId='419679539772-9qdng7lebu4li8jtgi1cne59728m8849.apps.googleusercontent.com'> */}
     <App />
-    </GoogleOAuthProvider>
+    {/* </GoogleOAuthProvider> */}
   </React.StrictMode>,
 )
